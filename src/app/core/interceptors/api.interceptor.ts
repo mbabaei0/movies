960f7b9c596097daed3e5e7c602640736d748d60
Mbabaei\mohammad <mbabaei0@gmail.com>
2024-05-27 19:56:24 +0200

    fix: httppClient bug

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -3,14 +3,12 @@ import { Observable } from "rxjs"
 import { environment } from "../../../environments/environment";
 
 export function apiKeyInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-  console.log(req.url);
-  const clonedRequest = req.clone();
   let params = req.params ? req.params : new HttpParams();
-  params = params.set('apiKey', environment.API_KEY);
 
+  params = params.set('apiKey', environment.API_KEY);
   const apiReq = req.clone({
     url: `${environment.API_URL}/${req.url}`,
     params
   });
-  return next(clonedRequest);
+  return next(apiReq);
 }
